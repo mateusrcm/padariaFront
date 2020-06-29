a079358899c016a493e3c860e295032afe596150
mateusrcm <mateus.rc.martins@gmail.com>
2020-06-29 18:11:59 -0300

    #system done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,155 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import {
+  NzTableSortFn,
+  NzTableSortOrder,
+  NzTableFilterList,
+  NzTableFilterFn,
+} from "ng-zorro-antd/table";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { NzMessageService } from "ng-zorro-antd/message";
+
+export type GenericObject = { [key: string]: any };
+
+interface DataItem {
+  name: string;
+  age: number;
+  address: string;
+}
+
+interface ColumnItem {
+  name: string;
+  sortOrder?: NzTableSortOrder;
+  sortFn?: NzTableSortFn;
+  listOfFilter?: NzTableFilterList;
+  filterFn?: NzTableFilterFn;
+  filterMultiple?: boolean;
+  sortDirections?: NzTableSortOrder[];
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'padariaFront';
+export class AppComponent implements OnInit, OnDestroy {
+  listOfColumns: ColumnItem[] = [
+    {
+      name: "Name",
+    },
+    {
+      name: "Description",
+    },
+    {
+      name: "Price",
+    },
+    {
+      name: "Quantity",
+    },
+    {
+      name: "Operations",
+    },
+  ];
+
+  obj: GenericObject = {
+    name: "",
+    description: "",
+    price: 0,
+    quantity: 0,
+  };
+  isVisible: boolean = false;
+  listOfData: GenericObject[] = [];
+
+  destroy$: Subject<void> = new Subject<void>();
+
+  constructor(public http: HttpClient, private message: NzMessageService) {}
+
+  ngOnInit(): void {
+    this.searchAll();
+  }
+
+  searchAll(
+    url: string = "http://localhost:5000/api/v1/products/",
+    options: any = {}
+  ): void {
+    this.http.get<any>(url, options).subscribe((observer: any) => {
+      if (!observer.success)
+        this.message.create("error", "Sorry :( an error has ocurred");
+
+      this.listOfData = observer.data;
+    });
+  }
+
+  save(url: string = "http://localhost:5000/api/v1/products/"): void {
+    if (this.obj._id) {
+      this.http
+        .put(`${url}${this.obj._id}`, this.obj)
+        .subscribe((observer: any) => {
+          if (observer.success)
+            this.message.create("success", "Succesfully updated data");
+          else this.message.create("error", "Sorry :( an error has ocurred");
+
+          this.clearObj();
+          this.searchAll();
+        });
+    } else {
+      this.http.post(url, this.obj).subscribe((observer: any) => {
+        if (observer.success)
+          this.message.create("success", "Succesfully created data");
+        else this.message.create("error", "Sorry :( an error has ocurred");
+
+        this.clearObj();
+        this.searchAll();
+      });
+    }
+  }
+
+  openCrudModal(): void {
+    this.isVisible = true;
+  }
+
+  clearObj(): void {
+    this.obj = {
+      name: "",
+      description: "",
+      price: 0,
+      quantity: 0,
+    };
+  }
+
+  handleOk(): void {
+    this.isVisible = false;
+    this.save();
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  editObj(data): void {
+    this.isVisible = true;
+    this.obj = data;
+  }
+
+  deleteObj(
+    data,
+    url: string = "http://localhost:5000/api/v1/products/"
+  ): void {
+    this.http.delete(`${url}${data._id}`).subscribe((observer: any) => {
+      if (observer.success)
+        this.message.create("success", "Succesfully removed data");
+      else this.message.create("error", "Sorry :( an error has ocurred");
+
+      this.searchAll();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  formatterDollar = (value: number) => `$ ${value}`;
+
+  parserDollar = (value: string) => value.replace("$ ", "");
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NZ_I18N } from "ng-zorro-antd/i18n";
+import { en_US } from "ng-zorro-antd/i18n";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+
+import { NzDividerModule } from "ng-zorro-antd/divider";
+import { NzTableModule } from "ng-zorro-antd/table";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { NzInputNumberModule } from "ng-zorro-antd/input-number";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzPopconfirmModule } from "ng-zorro-antd/popconfirm";
+import { NzMessageModule } from "ng-zorro-antd/message";
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    NzPopconfirmModule,
     BrowserModule,
-    AppRoutingModule
+    NzTableModule,
+    NzMessageModule,
+    NzButtonModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzLayoutModule,
+    NzGridModule,
+    NzDividerModule,
+    AppRoutingModule,
+    NzModalModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
